34d380810ad8db90a6111fb8d5e03bb650f009dc
Fabian Wetzel <fabian.wetzel@vattenfall.de>
2025-01-03 17:04:55 +0100

    [WIP] conversion to vue3: added vuetify

---
+++ edienergyviewer.client/src/components/DocumentsFilter.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+</script>
+
+<template>
+<v-card title="Card title" text="..."></v-card>
+</template>
---
+++ edienergyviewer.client/src/components/DocumentsList.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Documents List</h1>
+</template>
